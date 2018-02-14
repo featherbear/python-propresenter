ba26e350cee4b324593ab5bac6d892bba7247a0e
Andrew Wong <andrew12345@live.com>
2018-02-15 00:56:19 +1100

    Stubs for Transitions and Effects for media elements

--- propresenter/files/pro6pl.py
+++ propresenter/files/pro6pl.py
@@ -119,34 +119,35 @@ class File(XML):
             def __init__(elem, *args):
                 class add:
                     @staticmethod
-                    def audio(audioPath):
+                    def audio(audioPath, name=None):
                         __mediaObject = mutagen.File(audioPath)
                         if __mediaObject is None:
                             raise Exception("Bad media file")
                         __mediaObjectDuration = int(__mediaObject.info.length * 600)
+                        __displayName = (name+".") if name else os.path.basename(audioPath)
 
                         root = elem.data["array"][0]
                         data = {
                             "@UUID": uuid(),
-                            "@displayName": os.path.basename(audioPath),  # doesn't matter
-                            "@actionType": 0,
-                            "@enabled": 1,
-                            "@timeStamp": 0,
-                            "@delayTime": 0,
-                            "@tags": "",
-                            "@nextCueUUID": "00000000-0000-0000-0000-000000000000",
-                            "behavior": "1",
-                            "alignment": "4",
-                            "dateAdded": "",
+                            "@displayName": __displayName,
+                            "@actionType": 0, # ???
+                            "@enabled": 1, # ???
+                            "@timeStamp": 0, # ???
+                            "@delayTime": 0, # ???
+                            "@tags": "", # ???
+                            "@nextCueUUID": "00000000-0000-0000-0000-000000000000", # ???
+                            "behavior": "1", # ???
+                            "alignment": "4", # ???
+                            "dateAdded": "", # ???
                             "@__order__": self.currentOrder + 1,
                             "RVAudioElement": [{
                                 "@rvXMLIvarName": "element",
                                 "@volume": 1,
                                 "@playRate": 1,
                                 "@loopBehaviour": 0,
-                                "@audioType": 0,
+                                "@audioType": 0, # ???
                                 "@inPoint": 0,
-                                "@outPoint": 0,
+                                "@outPoint": __mediaObjectDuration,
                                 "@displayName": os.path.basename(audioPath),  # doesn't matter
                                 "@source": urllib.parse.quote(audioPath),
                                 "array": [{
@@ -164,16 +165,17 @@ class File(XML):
                         # no return
 
                     @staticmethod
-                    def video(videoPath):
+                    def video(videoPath, name=None):
                         __mediaObject = mutagen.File(videoPath)
                         if __mediaObject is None:
                             raise Exception("Bad media file")
                         __mediaObjectDuration = int(__mediaObject.info.length * 600) # timeScale = 600
+                        __displayName = (name+".") if name else os.path.basename(videoPath)
 
                         root = elem.data["array"][0]
                         data = {
                             "@UUID": uuid(),
-                            "@displayName": os.path.basename(videoPath),  # doesn't matter
+                            "@displayName": __displayName,
                             "@actionType": 0,
                             "@enabled": 1,
                             "@timeStamp": 0,
@@ -185,9 +187,31 @@ class File(XML):
                             "@dateAdded": "",
                             "@__order__": self.currentOrder + 1,
                             "RVVideoElement": [{
-                                "@UUID": None,
                                 "@rvXMLIvarName": "element",
-                                "@displayName": os.path.basename(videoPath),  # doesn't matter
+                                "@UUID": None,
+
+                                "@displayName": os.path.basename(videoPath),
+                                "@source": urllib.parse.quote(videoPath),
+
+                                "@scaleBehavior": 0,
+                                    # 0 - Scale to Fit
+                                    # 1 - Scale to Fill
+                                    # 2 - Stretch to Fit
+
+                                "@flippedHorizontally": "false",
+                                "@flippedVertically": "false",
+
+                                "@audioVolume": "1",
+                                "@playRate": "1",
+                                "@playbackBehavior": "0",
+
+                                "@timeScale": "600", # WHY THOUGH???
+                                "@inPoint": "0",  #
+                                "@outPoint": __mediaObjectDuration,  #
+                                "@endPoint": __mediaObjectDuration,  #
+
+                                "@fieldType": "0",
+
                                 # "@displayDelay": 0,
                                 # "@locked": 0,
                                 # "@persistent": 0,
@@ -199,37 +223,14 @@ class File(XML):
                                 # "@drawingStroke": 0,
                                 # "@fillColor": "1 1 1 1",
                                 # "@rotation": 0,
-                                "@source": urllib.parse.quote(videoPath),
+                                # "@manufactureURL": "",
+                                # "@manufactureName": "",
+                                # "@format": "",
+                                # "@scaleSize": "{1.0, 1.0}",
+                                # "@imageOffset": "{0.0, 0.0}",
+                                # "@frameRate": "23.9760246276855"
+                                # "@naturalSize": "{1920, 1080}",
 
-                                "@scaleBehavior": 0,
-                                    # 0 - Scale to Fit
-                                    # 1 - Scale to Fill
-                                    # 2 - Stretch to Fit
-                                #"@manufactureURL": "",
-                                #"@manufactureName": "",
-                                #"@format": "",
-                                #"@scaleSize": "{1.0, 1.0}",
-                                #"@imageOffset": "{0.0, 0.0}",
-
-                                #"@frameRate": "23.9760246276855", #???????????/
-
-
-
-
-                                "@flippedHorizontally": "false",
-                                "@flippedVertically": "false",
-
-                                "@audioVolume": "1",
-                                "@playRate": "1",
-                                "@playbackBehavior": "0", # STOP on finish
-
-                                "@timeScale": "600", # WHY THOUGH???
-                                "@inPoint": "0",  #
-                                "@outPoint": __mediaObjectDuration,  #
-                                "@endPoint": __mediaObjectDuration,  #
-
-                                #"@naturalSize": "{1920, 1080}",  #
-                                "@fieldType": "0",
                                 "RVRect3D": [{
                                     "@rvXMLIvarName": "position",
                                     "#text": "{0 0 0 0 0}",
@@ -256,37 +257,38 @@ class File(XML):
                             }]
                         }
                         data["RVVideoElement"][0]["@UUID"] = data["@UUID"].lower()
+
                         if "RVMediaCue" in root:
                             root["RVMediaCue"].append(data)
                         else:
                             root["RVMediaCue"] = [data]
 
-                        # an existing file might need the order of all the proceeding elements shifted
                         self.currentOrder += 1
-                        # return
 
                     @staticmethod
-                    def image(imagePath):
+                    def image(imagePath, name=None):
+                        __displayName = (name + ".") if name else os.path.basename(imagePath)
+
                         root = elem.data["array"][0]
                         data = {
                             "@UUID": uuid(),
-                            "@displayName": os.path.basename(imagePath),  # doesn't matter
-                                # on the playlist view, the extension is stripped away
-                                #   # "aaaaa." -> "aaaaa"
-                                # Full name shows up on the contiguous view
-                            "@actionType": 0,
+                            "@displayName": __displayName,
+                            "@actionType": 0, # ???
                             "@enabled": 1,
-                            "@timeStamp": 0,
-                            "@delayTime": 0,
+                            "@timeStamp": 0, # ???
+                            "@delayTime": 0, # ???
                             "@tags": "",
                             "@nextCueUUID": "00000000-0000-0000-0000-000000000000",
-                            "@behavior": 2,
+                            "@behavior": 2, # ???
                             "@alignment": 4,
-                            "@dateAdded": "",
+                                # 0 1 2
+                                # 3 4 5
+                                # 6 7 8
+                            "@dateAdded": "", # ???
                             "@__order__": self.currentOrder + 1,
                             "RVImageElement": [{
-                                "@UUID": None,
                                 "@rvXMLIvarName": "element",
+                                "@UUID": None,
                                 "@displayName": os.path.basename(imagePath),  # doesn't matter
                                 "@displayDelay": 0,
                                 "@locked": 0,
@@ -303,6 +305,7 @@ class File(XML):
                                 "@flippedHorizontally": "false",
                                 "@flippedVertically": "false",
                                 "@scaleBehavior": 0,
+                                "@manufactureURL": "",
                                 "@manufactureName": "",
                                 "@format": "",
                                 "@scaleSize": "{1.0, 1.0}",
@@ -351,10 +354,10 @@ class File(XML):
                             "@displayName": os.path.basename(documentPath),  # doesn't matter
                             "@filePath": urllib.parse.quote(documentPath),
                             "@selectedArrangementID": "",
-                            "@actionType": 0,
+                            "@actionType": 0, # ???
                             "@enabled": 1,
-                            "@timeStamp": 0,
-                            "@delayTime": 0,
+                            "@timeStamp": 0, # ???
+                            "@delayTime": 0, # ???
                             "@__order__": self.currentOrder + 1,
                         }
                         if "RVDocumentCue" in root:
@@ -419,6 +422,214 @@ class File(XML):
             # Contains RVVideoElement, RVImageElement
             def __init__(self, *args):
                 print("  INIT MEDIA")
+
+                '''
+                # Transitions
+
+                CUT
+                transitionType: 10
+
+                DISSOLVE
+                transitionType: 0
+                transitionDuration: 0.0-5.0
+
+                FADE BLACK
+                transitionType: 7
+                transitionDuration: 0.0-5.0
+
+                FLIP
+                transitionType: 2            
+                transitionDuration: 0.0-5.0
+
+                SWAP
+                transitionType: 4
+                transitionDuration: 0.0-5.0
+
+                CUBE
+                transitionType: 1
+                transitionDuration: 0.0-5.0
+
+                DOOR
+                transitionType: 3
+                transitionDuration: 0.0-5.0
+
+                IRIS
+                transitionType: 8
+                transitionDuration: 0.0-5.0
+
+                RIPPLE
+                transitionType: 6
+                transitionDuration: 0.0-5.0
+
+                PUSH
+                transitionType: UP 133, RIGHT 130, DOWN 132, LEFT 131
+                transitionDuration: 0.0-5.0
+
+                WIPE
+                transitionType: LEFT 114, RIGHT 113, DOWN 111, UP 116, TOPRIGHT 115, BOTTOMRIGHT 110, BOTTOMLEFT 112, TOPLEFT 117
+                transitionDuration: 0.0-5.0
+
+                COVER
+                transitionType: LEFT 124, RIGHT 123, DOWN 121, UP 126, TOPRIGHT 125, BOTTOMRIGHT 120, BOTTOMLEFT 122, TOPLEFT, 127
+                transitionDuration: 0.0-5.0
+
+                REVEAL
+                transitionType: LEFT 144, RIGHT 143, DOWN 141, UP 146, TOPRIGHT 145, BOTTOMRIGHT 140, BOTTOMLEFT 142, TOPLEFT, 147
+                transitionDuration: 0.0-5.0
+
+                FLY IN
+                transitionType: LEFT 96, RIGHT 94, DOWN 92, UP 98, TOPRIGHT 97, BOTTOMRIGHT 91, BOTTOMLEFT 93, TOPLEFT 99, CENTER 95
+                transitionDuration: 0.0-5.0
+                motionEnabled: true/false
+                motionSpeed: 10-[speed] => 0.0-10
+
+                ZOOM IN
+                transitionType: LEFT 106, RIGHT 104, DOWN 102, UP 108, TOPRIGHT 107, BOTTOMRIGHT 101, BOTTOMLEFT 103, TOPLEFT 109, CENTER 105
+                transitionDuration: 0.0-5.0
+                motionEnabled: true/false
+                motionSpeed: 10-[speed] => 0.0-10
+                '''  # Transition #
+
+                class effects(list):
+                   pass
+
+                '''
+                # Effects
+
+                #Adjust Color
+                {
+                "@UUID": "D4C199A9-64D7-4CCB-8C26-443E7A9C5C03",
+                    "@displayName": "Adjust Color",
+                    "array": [{
+                        "@rvXMLIvarName": "effectVariables",
+                      "RVEffectFloatVariable": [{
+                      "@name": "hue"    ,
+                          "@type" : "1",
+                "@min" : "-3.14159274",
+                "@max": "3.14159274",
+                "@defValue": "0",
+                "@value": "0"
+                      },{
+                          "@name": "saturation","@type":"1","@min":"0","@max":"2","@defValue":"1","@value":"1"
+
+                      },
+                          {"@name":"brightness","@type":"1","@min":"-1","@max":"1","@defValue":"0","@value":"0"},
+                          {"@name":"contrast","@type":"1","@min":"0","@max":"2","@defValue":"1","@value":"1"}]
+                    }]
+                }
+
+                #Blur
+                {
+                "@UUID":"D84CE4A3-D1C3-4F7C-AA75-4C645A3621F2","@displayName":"Blur",
+                    "array": [{
+                "@rvXMLIvarName":"effectVariables",
+                        "RVEffectFloatVariable": [{
+                "@name":"blurAmount","@type":"1","@min":"0","@max":"1","@defValue":"0.5","@value":"0.5"
+                        }]
+                    }]
+                }
+
+                #Color Filter
+                {
+                "@UUID":"85A535EF-4EFC-4105-B8C7-C65C53376B87","@displayName":"Color Filter",
+                    "array": [{
+                "@rvXMLIvarName":"effectVariables",
+                        "RVEffectFloatVariable": [{
+                "@name":"pickedColor","@type":"2","@value":"1 1 1 1"
+                        }]
+                    }]
+                }
+
+                #Color Invert
+                {
+                "@UUID":"EDF55F11-1A1E-4BF3-9718-FA2D3731626D","@displayName":"Color Invert",
+                    "array": [{
+                "@rvXMLIvarName":"effectVariables"
+                    }]
+                }
+
+                #Edge Blur
+                {
+                "@UUID":"AF83B133-D6A1-487C-BD9E-8579FA49B4E0","@displayName":"Edge Blur",
+                    "array": [{
+                        "@rvXMLIvarName": "effectVariables",
+                        "RVEffectFloatVariable": [{
+                            "@name": "blurAmount", "@type": "1", "@min": "0", "@max": "1", "@defValue": "0.5", "@value": "0.5"
+                        },{
+                "@name":"blurRadius","@type":"1","@min":"0","@max":"1","@defValue":"0.5","@value":"0.25"
+                        }]
+                    }]
+                }
+
+                #Gray Invert
+                {
+                "@UUID":"4FDA3849-C39D-4269-AADA-B963944900B7","@displayName":"Gray Invert",
+                    "array": [{
+                "@rvXMLIvarName":"effectVariables"
+                    }]
+                }
+
+                #Halftone
+                {
+                "@UUID":"ADF80D5E-FD27-47BD-AFA6-8D4167364567","@displayName":"Halftone",
+                    "array": [{
+                        "@rvXMLIvarName": "effectVariables",
+                        "RVEffectFloatVariable": [{
+                            "@name":"sharpnessValue","@type":"1","@min":"0","@max":"1","@defValue":"0.5","@value":"0.5"
+                        },{
+                "@name":"gcrValue","@type":"1","@min":"0","@max":"2","@defValue":"1","@value":"1"
+                        },{
+                "@name":"ucrValue","@type":"1","@min":"0","@max":"1","@defValue":"0.5","@value":"0.5"
+                        }]
+                    }]
+                }
+
+                #Heat Signature
+                {
+                "@UUID":"F3F254AF-BF83-412D-B802-51E42A25FDF9","@displayName":"Heat Signature",
+                    "array": [{
+                "@rvXMLIvarName":"effectVariables"
+                    }]
+                }
+
+
+                #Old Film
+                {
+                "@UUID":"1E5BAD9B-205C-404D-869B-2B38C33FD28F","@displayName":"Old Film",
+                    "array": [{
+                        "@rvXMLIvarName": "effectVariables",
+                        "RVEffectFloatVariable": [{
+                            "@name": "sharpnessValue", "@type": "1", "@min": "0", "@max": "1", "@defValue": "0.5", "@value": "0.5"
+                        },{
+                "@name":"gcrValue","@type":"1","@min":"0","@max":"2","@defValue":"1","@value":"1"
+                        },{
+                "@name":"ucrValue","@type":"1","@min":"0","@max":"1","@defValue":"0.5","@value":"0.5"
+                        }]
+                    }]
+                }
+
+                #Sepia
+                {
+                "@UUID":"06157C8E-D971-4F06-9ECA-2663664E22A6","@displayName":"Sepia",
+                    "array": [{
+                "@rvXMLIvarName":"effectVariables"
+                    }]
+                }
+
+                #Vignette
+                {
+                "@UUID":"42E83016-41DC-43AB-8061-9A93256DDA46","@displayName":"Vignette",
+                    "array": [{
+                "@rvXMLIvarName":"effectVariables",
+                        "RVEffectFloatVariable": [{
+                "@name":"radius","@type":"1","@min":"0","@max":"100","@defValue":"50","@value":"50"
+                        }]
+                    }]
+                }
+                '''  # Effects
+
+
+                self.effects = effects()
 
             pass
 
